5625178f3fd32e323fcca1881f981008586ddd74
dilanunver <dunver33@gmail.com>
2021-04-22 17:00:30 +0300

    adding page screen and showing true/false answers

--- src/App.js
+++ src/App.js
@@ -2,38 +2,43 @@ import React, { useState, useEffect } from 'react';
 import Questions from './Questions'
 
 const url = 'https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple'
+
 function App() {
   const [questions, setQuestions] = useState([])
 
   const fetchData = async () => {
     const response = await fetch(url);
-    const jsonResponse = await response.json()
+    const jsonResponse = await response.json() //then, catch
 
     const result = jsonResponse.results
+
     result.forEach(function (item) {
       item.isAnswered = false;
       item.isCorrect = false;
-      item.isClicked = null;
+      item.clickedOption = null;
+      item.id = new Date().getTime()
     })
     setQuestions(result)
+
   }
+  console.log(questions)
   const checkTheUser = (index, answer) => {
     const whatTheUserAnswered = [...questions]
     const indexAnswered = whatTheUserAnswered[index]
+    console.log(index)
     if (indexAnswered.incorrect_answers.includes(answer)) {
       indexAnswered.isAnswered = true;
-
     }
     if (indexAnswered.correct_answer === answer) {
       indexAnswered.isCorrect = true
     }
+    indexAnswered.clickedOption = answer
 
-    if (answer === indexAnswered.correct_answer) {
-      indexAnswered.isClicked = answer
-    }
+
     setQuestions(whatTheUserAnswered)
 
   }
+
   useEffect(() => {
     fetchData()
   }, [])
--- src/Questions.js
+++ src/Questions.js
@@ -1,44 +1,71 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Questions = ({ result, checkTheUser }) => {
 
+  const [currentQuestion, setCurrentQuestion] = useState(0)
 
 
+  if (result.length === 0) {
+    return (
+      <h4>Loading</h4>
+    )
+  }
+  const nextButton = () => {
+    let newQuestion = currentQuestion + 1
+    let lastQuestion = result.length - 1
+    console.log(newQuestion)
+    console.log(lastQuestion)
+    if (newQuestion > lastQuestion) {
+      return setCurrentQuestion(0)
+    }
+    return setCurrentQuestion(newQuestion)
+
+
+  }
+
+  if (result[currentQuestion].incorrect_answers.length === 3) {
+    result[currentQuestion].incorrect_answers.push(result[currentQuestion].correct_answer)
+    result[currentQuestion].incorrect_answers.sort(() => Math.random() - 0.5)
+  }
   return (
     <div>
-      {result.map((item, index) => {
-        const { question, correct_answer, incorrect_answers, isAnswered, isCorrect, isClicked } = item;
-        if (incorrect_answers.length === 3) {
-          incorrect_answers.push(correct_answer)
-          incorrect_answers.sort(() => Math.random() - 0.5)
-
+      <h5>question {currentQuestion + 1}</h5>
+      <h4>{result[currentQuestion].question}</h4>
+      {result[currentQuestion].incorrect_answers.map((option) => {
+        let buttonStyle = {};
+        if (result[currentQuestion].isAnswered && result[currentQuestion].clickedOption === option) {
+          if (result[currentQuestion].isCorrect) {
+            buttonStyle = { backgroundColor: 'green', padding: '10px', color: 'white' }
+          } else {
+            buttonStyle = { backgroundColor: 'red', padding: '10px', color: 'white' }
+          }
         }
-
         return (
+
           <div>
-            <h4>{question}</h4>
-            {incorrect_answers.map((incorrectAnswer) => {
-              console.log(isClicked)
-              console.log(incorrectAnswer)
+            <button disabled={result[currentQuestion].isAnswered}
+              style={buttonStyle}
+              onClick={() => checkTheUser(currentQuestion, option)}
+            ><span>
+                {option}
+              </span>
 
-              return (
-                <button disabled={isAnswered} onClick={() => checkTheUser(index, incorrectAnswer)}
-                ><span style={isCorrect ? isClicked === incorrectAnswer ? { backgroundColor: 'green', padding: '10px', color: 'white' } : {} : {}} >
-                    {incorrectAnswer}
-                  </span></button>
-              )
-
-            })}
-
+            </button>
           </div>
         )
-
       })}
-    </div>
+
+      <button onClick={nextButton}>
+        next
+      </button>
+    </div >
+
   )
 
-
 }
+
+
+
 
 
 export default Questions
